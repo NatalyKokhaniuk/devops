6280ff8cf48b5818de9fe90106798044cab17d3c
Nataly Kokhaniuk <kokhaniuk.nataliia@chnu.edu.ua>
2026-06-19 12:18:07 +0300

    chore: reset branch 1 to lab1

--- helm/deploy.ps1
+++
@@ -1,6 +0,0 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-helm install mydb bitnami/postgresql -f .\helm\postgres-values.yaml
-helm upgrade --install myapp .\helm\rest-api-chart
-kubectl get pods
-helm list
