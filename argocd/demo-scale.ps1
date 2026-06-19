52ccd20cee923ff6f218c08925001d16e40ac9ee
Nataly Kokhaniuk <kokhaniuk.nataliia@chnu.edu.ua>
2026-06-19 12:11:49 +0300

    chore: remove lab4 leftovers from branch 1

--- argocd/demo-scale.ps1
+++
@@ -1,4 +0,0 @@
-(Get-Content .\k8s\deployment.yml) -replace 'replicas: 1', 'replicas: 3' | Set-Content .\k8s\deployment.yml
-git add .\k8s\deployment.yml
-git commit -m "Scale replicas to 3"
-git push
